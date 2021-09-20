7b10584312cb7565e5225c7301583b15323b0f43
lori3f6 <lori3f6@gmail.com>
2021-09-20 13:54:33 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/LanguaLab/MelonAuth2-plugin-api.git
+cd MelonAuth2-plugin-api
+git pull
+git checkout 1.2.1-alpha
+mvn install
+cd ..
+mvn package
---
+++ readme.md
@@ -0,0 +1,4 @@
+##Build
+```
+sh ./build.sh
+```
